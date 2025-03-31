ac2851809899ee5c336349462275867f9c01bf55
NikolaHead <hprowriter@gmail.com>
2025-04-01 01:02:41 +0200

    Delete styles.js

--- styles.js
+++
@@ -1,6 +0,0 @@
-// Inicijalizacija proizvoda
-const products = [
-    { id: 1, name: "Pametan telefon", price: 1500, oldPrice: 1800, category: "electronics", image: "https://via.placeholder.com/200", description: "Pametan telefon sa naprednim funkcijama." },
-    { id: 2, name: "Sportske tenisice", price: 600, oldPrice: 800, category: "clothing", image: "https://via.placeholder.com/200", description: "Komforne sportske tenisice za svaki dan." },
-    { id: 3, name: "Luksuzni sat", price: 2500, oldPrice: 3000, category: "accessories", image: "https://via.placeholder.com/200", description: "Elegantan sat sa savremenim dizajnom." },
-    { id: 4, name: "Laptop", price: 3000, oldPrice: 3500, category: "
